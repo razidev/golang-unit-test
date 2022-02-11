10c9bde154251af9e1d524f8ec0db797b6de4459
razidev <razisyahputro@gmail.com>
2022-02-11 15:58:26 +0700

    add more test Fatal

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -5,12 +5,25 @@ import (
 	"testing"
 )
 
+/* do not use panic, use t.Fail(), t.FailNow(),
+t.Error(message string), t.Fatal(message string),
+t.FatalNow(message string) instead */
+
 func TestHeloWolrd1(t *testing.T) {
 	result := HelloWorld("Razi")
 
 	if result != "Hello Razi" {
-		// t.Fail()
 		t.Error("Error, result should be 'Hello Razi'")
 	}
 	fmt.Println("TestHeloWolrd1 done")
+}
+
+func TestHeloWolrd2(t *testing.T) {
+	result := HelloWorld("Hulk")
+
+	if result != "HelloHulk" {
+		// t.FailNow()
+		t.Fatal("Error, result should be 'HelloHulk'")
+	}
+	fmt.Println("TestHeloWolrd2 done")
 }
